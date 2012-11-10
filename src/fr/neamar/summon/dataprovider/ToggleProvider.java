02727ad4afa2c54f90ed8a50db124c763f3a88b8
Dorvaryn <benjamin@odai.homelinux.net>
2012-11-11 00:34:10 +0100

    Now available even without phone, wifi or bluetooth support (fix #16)

--- src/fr/neamar/summon/dataprovider/ToggleProvider.java
+++ src/fr/neamar/summon/dataprovider/ToggleProvider.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import fr.neamar.summon.R;
 import fr.neamar.summon.holder.Holder;
 import fr.neamar.summon.holder.ToggleHolder;
@@ -11,38 +12,41 @@ import fr.neamar.summon.holder.ToggleHolder;
 public class ToggleProvider extends Provider {
 	private ArrayList<ToggleHolder> toggles = new ArrayList<ToggleHolder>();
 
-	public ToggleProvider(Context context) {
+	public ToggleProvider(final Context context) {
 		super();
 		holderScheme = "toggle://";
-		Thread thread = new Thread(null, initTogglesList);
+		Thread thread = new Thread(null, new Runnable() {
+			public void run() {
+				PackageManager pm = context.getPackageManager();
+				if(pm.hasSystemFeature(PackageManager.FEATURE_WIFI)){
+					toggles.add(createHolder("Wifi", "wifi", R.drawable.toggle_wifi));
+				}
+				if(pm.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)){
+					toggles.add(createHolder("Bluetooth", "bluetooth",
+							R.drawable.toggle_bluetooth));
+				}
+				toggles.add(createHolder("Silent", "silent",
+						R.drawable.toggle_silent));
+				// toggles.add(createHolder("GPS", "gps", R.drawable.toggle_gps));
+				// toggles.add(createHolder("Mobile network data", "data",
+				// R.drawable.toggle_data));
+			}
+
+			private ToggleHolder createHolder(String name, String settingName,
+					int resId) {
+				ToggleHolder holder = new ToggleHolder();
+				holder.id = holderScheme + name.toLowerCase();
+				holder.name = "Toggle: " + name;
+				holder.nameLowerCased = holder.name.toLowerCase();
+				holder.settingName = settingName;
+				holder.icon = resId;
+
+				return holder;
+			}
+		});
 		thread.setPriority(Thread.NORM_PRIORITY + 1);
 		thread.start();
 	}
-
-	protected Runnable initTogglesList = new Runnable() {
-		public void run() {
-			toggles.add(createHolder("Wifi", "wifi", R.drawable.toggle_wifi));
-			// toggles.add(createHolder("GPS", "gps", R.drawable.toggle_gps));
-			toggles.add(createHolder("Bluetooth", "bluetooth",
-					R.drawable.toggle_bluetooth));
-			toggles.add(createHolder("Silent", "silent",
-					R.drawable.toggle_silent));
-			// toggles.add(createHolder("Mobile network data", "data",
-			// R.drawable.toggle_data));
-		}
-
-		private ToggleHolder createHolder(String name, String settingName,
-				int resId) {
-			ToggleHolder holder = new ToggleHolder();
-			holder.id = holderScheme + name.toLowerCase();
-			holder.name = "Toggle: " + name;
-			holder.nameLowerCased = holder.name.toLowerCase();
-			holder.settingName = settingName;
-			holder.icon = resId;
-
-			return holder;
-		}
-	};
 
 	public ArrayList<Holder> getResults(String query) {
 		ArrayList<Holder> holders = new ArrayList<Holder>();
