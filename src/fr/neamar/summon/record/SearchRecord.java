a19fb64fb7a061c1371946733e4a27997a42503a
Neamar <neamar@neamar.fr>
2012-11-10 21:24:42 +0100

    Handle missing google search (fix #3)

--- src/fr/neamar/summon/record/SearchRecord.java
+++ src/fr/neamar/summon/record/SearchRecord.java
@@ -1,10 +1,13 @@
 package fr.neamar.summon.record;
 
 import android.app.SearchManager;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import fr.neamar.summon.R;
 import fr.neamar.summon.holder.SearchHolder;
 
@@ -33,7 +36,16 @@ public class SearchRecord extends Record {
 		Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
 		search.putExtra(SearchManager.QUERY, searchHolder.query);
 		search.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		context.startActivity(search);
+		
+		try {
+			context.startActivity(search);
+		} catch (ActivityNotFoundException e) {
+			//This exception gets thrown if Google Search has been deactivated:
+			Uri uri = Uri.parse("http://www.google.com/#q=" + searchHolder.query);
+			search = new Intent(Intent.ACTION_VIEW, uri);
+			search.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			context.startActivity(search);
+		}
 	}
 
 }
