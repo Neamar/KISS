83ca3fe1edf05eaa53b5d16db024373dda31fd73
Neamar <neamar@neamar.fr>
2012-11-10 11:25:00 +0100

    Comment on UpdateHandler

--- src/fr/neamar/summon/UpdateHandler.java
+++ src/fr/neamar/summon/UpdateHandler.java
@@ -3,13 +3,19 @@ package fr.neamar.summon;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
+/**
+ * This class gets called when an application is created or removed on the system
+ * 
+ * We then recreate our data set.
+ * 
+ * @author dorvaryn
+ *
+ */
 public class UpdateHandler extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context ctx, Intent intent) {
-		Log.w("wtf", "received something " + intent.getAction());
 		SummonApplication.resetDataHandler(ctx);
 	}
 
