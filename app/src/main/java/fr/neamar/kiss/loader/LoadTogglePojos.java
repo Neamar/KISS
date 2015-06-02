51e96cd57dd2f7169ec425f371bcf5f1a3b285d6
Neamar <neamar@neamar.fr>
2015-06-02 18:56:43 +0200

    Localized settings

--- app/src/main/java/fr/neamar/kiss/loader/LoadTogglePojos.java
+++ app/src/main/java/fr/neamar/kiss/loader/LoadTogglePojos.java
@@ -36,7 +36,7 @@ public class LoadTogglePojos extends LoadPojos<TogglePojo> {
     private TogglePojo createPojo(String name, String settingName, int resId) {
         TogglePojo pojo = new TogglePojo();
         pojo.id = pojoScheme + name.toLowerCase();
-        pojo.name = "Toggle: " + name;
+        pojo.name = context.getString(R.string.toggles_prefix) + name;
         pojo.nameLowerCased = pojo.name.toLowerCase();
         pojo.settingName = settingName;
         pojo.icon = resId;
