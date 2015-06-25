a1bbec86f7068f2113beead2e100da4d579c17d3
Neamar <neamar@neamar.fr>
2015-06-25 18:54:17 +0200

    Also return for search on 'settings'

--- app/src/main/java/fr/neamar/kiss/dataprovider/SettingProvider.java
+++ app/src/main/java/fr/neamar/kiss/dataprovider/SettingProvider.java
@@ -5,14 +5,17 @@ import android.content.Context;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import fr.neamar.kiss.R;
 import fr.neamar.kiss.loader.LoadSettingPojos;
 import fr.neamar.kiss.pojo.Pojo;
 import fr.neamar.kiss.pojo.SettingPojo;
 
 public class SettingProvider extends Provider<SettingPojo> {
+    private final String settingName;
 
     public SettingProvider(Context context) {
         super(new LoadSettingPojos(context));
+        settingName = context.getString(R.string.settings_prefix).toLowerCase();
     }
 
     public ArrayList<Pojo> getResults(String query) {
@@ -28,6 +31,10 @@ public class SettingProvider extends Provider<SettingPojo> {
                 relevance = 10;
             else if (settingNameLowerCased.contains(" " + query))
                 relevance = 5;
+            else if (settingName.startsWith(query)) {
+                // Also display for a search on "settings" for instance
+                relevance = 4;
+            }
 
             if (relevance > 0) {
                 setting.displayName = setting.name.replaceFirst(
