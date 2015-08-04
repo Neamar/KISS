fa9b44dd20cffa3e9e6b233cf844e45c47c92907
Neamar <neamar@neamar.fr>
2015-08-04 15:29:25 +0200

    Display search results in history

--- app/src/main/java/fr/neamar/kiss/dataprovider/SearchProvider.java
+++ app/src/main/java/fr/neamar/kiss/dataprovider/SearchProvider.java
@@ -3,12 +3,14 @@ package fr.neamar.kiss.dataprovider;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import fr.neamar.kiss.loader.LoadSearchPojos;
 import fr.neamar.kiss.pojo.Pojo;
 import fr.neamar.kiss.pojo.SearchPojo;
 
 public class SearchProvider extends Provider<SearchPojo> {
+    public static final String SEARCH_SCHEME = "search://";
 
     public SearchProvider(Context context) {
         super(new LoadSearchPojos(context));
@@ -16,11 +18,20 @@ public class SearchProvider extends Provider<SearchPojo> {
 
     public ArrayList<Pojo> getResults(String query) {
         ArrayList<Pojo> pojos = new ArrayList<>();
+        pojos.add(getResult(query));
 
+        return pojos;
+    }
+
+    public Pojo findById(String id) {
+        return getResult(id.replaceFirst(Pattern.quote(SEARCH_SCHEME), ""));
+    }
+
+    public Pojo getResult(String query) {
         SearchPojo pojo = new SearchPojo();
+        pojo.id = "search://" + query;
         pojo.query = query;
         pojo.relevance = 10;
-        pojos.add(pojo);
-        return pojos;
+        return pojo;
     }
 }
