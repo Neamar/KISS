0c26ead8e953383e7f1498df0dc695e5752df20d
Neamar <neamar@neamar.fr>
2015-10-03 18:21:58 +0200

    F** me i'm dumbass

--- app/src/main/java/fr/neamar/kiss/result/AppResult.java
+++ app/src/main/java/fr/neamar/kiss/result/AppResult.java
@@ -1,6 +1,7 @@
 package fr.neamar.kiss.result;
 
 import android.annotation.TargetApi;
+import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -142,6 +143,16 @@ public class AppResult extends Result {
 
     @Override
     public void doLaunch(Context context, View v) {
-        throw new RuntimeException("LOLILOL");
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setComponent(className);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            // Application was just removed?
+            Toast.makeText(context, R.string.application_not_found, Toast.LENGTH_LONG).show();
+        }
     }
 }
