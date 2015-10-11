a4d70038198675f3e12198294cb5ca2cd9cc031e
nmitsou <nikos@neemo.gr>
2015-10-11 02:57:51 +0200

    Issue #175: Possible fix. Listening to MEDIA_MOUNTED / REMOVED. Apps on sdcard are detected. Tested on kitkat Issue #253: Add newly installed app on history. Tested on lollipop

--- app/src/main/java/fr/neamar/kiss/broadcast/NewAppInstalledHandler.java
+++ app/src/main/java/fr/neamar/kiss/broadcast/NewAppInstalledHandler.java
@@ -3,6 +3,9 @@ package fr.neamar.kiss.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import fr.neamar.kiss.KissApplication;
 
@@ -18,6 +21,14 @@ public class NewAppInstalledHandler extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
+
+        if ("android.intent.action.PACKAGE_ADDED".equals(intent.getAction())) {
+            //add new package to history
+            String packageName = intent.getData().getSchemeSpecificPart();
+            String className = ctx.getPackageManager().getLaunchIntentForPackage(packageName).getComponent().getClassName();
+            KissApplication.getDataHandler(ctx).addToHistory(ctx, "app://" + packageName+"/"+className);
+        }
+
         KissApplication.resetDataHandler(ctx);
     }
 
