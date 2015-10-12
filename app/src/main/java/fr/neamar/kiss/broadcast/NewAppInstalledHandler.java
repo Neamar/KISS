577e8c6be0a6d0b8f9d9d7e0b206ec28929bfecc
Neamar <neamar@neamar.fr>
2015-10-12 11:35:01 +0200

    Handle null case

--- app/src/main/java/fr/neamar/kiss/broadcast/NewAppInstalledHandler.java
+++ app/src/main/java/fr/neamar/kiss/broadcast/NewAppInstalledHandler.java
@@ -3,9 +3,6 @@ package fr.neamar.kiss.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.util.Log;
 
 import fr.neamar.kiss.KissApplication;
 
@@ -23,10 +20,12 @@ public class NewAppInstalledHandler extends BroadcastReceiver {
     public void onReceive(Context ctx, Intent intent) {
 
         if ("android.intent.action.PACKAGE_ADDED".equals(intent.getAction())) {
-            //add new package to history
+            // Add new package to history
             String packageName = intent.getData().getSchemeSpecificPart();
             String className = ctx.getPackageManager().getLaunchIntentForPackage(packageName).getComponent().getClassName();
-            KissApplication.getDataHandler(ctx).addToHistory(ctx, "app://" + packageName+"/"+className);
+            if (className != null) {
+                KissApplication.getDataHandler(ctx).addToHistory(ctx, "app://" + packageName + "/" + className);
+            }
         }
 
         KissApplication.resetDataHandler(ctx);
