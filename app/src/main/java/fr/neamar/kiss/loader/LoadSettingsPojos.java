b4c55097b8b02eaea11b54746a970dc3688ae814
nmitsou <nikos@neemo.gr>
2016-02-12 12:37:47 +0100

    Adding nfc and tethering on settings

--- app/src/main/java/fr/neamar/kiss/loader/LoadSettingsPojos.java
+++ app/src/main/java/fr/neamar/kiss/loader/LoadSettingsPojos.java
@@ -28,7 +28,20 @@ public class LoadSettingsPojos extends LoadPojos<SettingsPojo> {
                 android.provider.Settings.ACTION_WIRELESS_SETTINGS, R.drawable.toggle_wifi));
         settings.add(createPojo(context.getString(R.string.settings_battery),
                 Intent.ACTION_POWER_USAGE_SUMMARY, R.drawable.setting_battery));
+        settings.add(createPojo(context.getString(R.string.settings_tethering), "com.android.settings",
+                "com.android.settings.TetherSettings", R.drawable.setting_tethering));
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            settings.add(createPojo(context.getString(R.string.settings_nfc),
+                    android.provider.Settings.ACTION_NFC_SETTINGS, R.drawable.setting_nfc));
+        }
         return settings;
+    }
+
+    private SettingsPojo createPojo(String name, String packageName, String settingName, int resId)
+    {
+        SettingsPojo pojo = this.createPojo(name, settingName, resId);
+        pojo.packageName = packageName;
+        return pojo;
     }
 
     private SettingsPojo createPojo(String name, String settingName, int resId) {
