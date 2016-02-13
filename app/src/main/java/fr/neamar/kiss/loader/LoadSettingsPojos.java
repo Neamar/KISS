62922ee5dc21d5b519a10f53abaed2179d69f537
nmitsou <nikos@neemo.gr>
2016-02-13 02:22:29 +0100

    Show nfc setting only if nfc is available

--- app/src/main/java/fr/neamar/kiss/loader/LoadSettingsPojos.java
+++ app/src/main/java/fr/neamar/kiss/loader/LoadSettingsPojos.java
@@ -2,6 +2,7 @@ package fr.neamar.kiss.loader;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -17,6 +18,7 @@ public class LoadSettingsPojos extends LoadPojos<SettingsPojo> {
 
     @Override
     protected ArrayList<SettingsPojo> doInBackground(Void... params) {
+        PackageManager pm = context.getPackageManager();
         ArrayList<SettingsPojo> settings = new ArrayList<>();
         settings.add(createPojo(context.getString(R.string.settings_airplane),
                 android.provider.Settings.ACTION_AIRPLANE_MODE_SETTINGS, R.drawable.setting_airplane));
@@ -30,7 +32,7 @@ public class LoadSettingsPojos extends LoadPojos<SettingsPojo> {
                 Intent.ACTION_POWER_USAGE_SUMMARY, R.drawable.setting_battery));
         settings.add(createPojo(context.getString(R.string.settings_tethering), "com.android.settings",
                 "com.android.settings.TetherSettings", R.drawable.setting_tethering));
-        if (android.os.Build.VERSION.SDK_INT >= 16) {
+        if ((android.os.Build.VERSION.SDK_INT >= 16) && (pm.hasSystemFeature(PackageManager.FEATURE_NFC))) {
             settings.add(createPojo(context.getString(R.string.settings_nfc),
                     android.provider.Settings.ACTION_NFC_SETTINGS, R.drawable.setting_nfc));
         }
