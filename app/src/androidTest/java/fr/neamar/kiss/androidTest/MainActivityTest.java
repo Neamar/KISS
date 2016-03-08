46831cd650e9f428825e167bdb2f6fdcacc723ec
Ismail <ismail-s.no-reply@github.com>
2016-03-08 17:53:08 +0000

    Add tests for some items appearing when searching

--- app/src/androidTest/java/fr/neamar/kiss/androidTest/MainActivityTest.java
+++ app/src/androidTest/java/fr/neamar/kiss/androidTest/MainActivityTest.java
@@ -1,27 +1,24 @@
 
 package fr.neamar.kiss.androidTest;
 
-import fr.neamar.kiss.R;
-import fr.neamar.kiss.MainActivity;
-
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Before;
-import org.junit.Test;
+
+import fr.neamar.kiss.MainActivity;
+import fr.neamar.kiss.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.action.ViewActions.typeText;
 
 @LargeTest
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-
-    private MainActivity mActivity;
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -30,12 +27,29 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     public void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
-        mActivity = getActivity();
+        getActivity();
     }
 
-    @Test
     public void testCanTypeTextIntoSearchBox() {
         onView(withId(R.id.searchEditText)).perform(typeText("Test"));
         onView(withId(R.id.searchEditText)).check(matches(withText("Test")));
+    }
+
+    public void testSearchResultAppears() {
+        onView(withId(R.id.searchEditText)).perform(typeText("blahblah"));
+        onView(withId(R.id.item_search_icon)).check(matches(isDisplayed()));
+        onView(withId(R.id.item_search_text)).check(matches(withText("Search for “blahblah”")));
+    }
+
+    public void testTorchToggleAppears() {
+        onView(withId(R.id.searchEditText)).perform(typeText("torch"));
+        onView(withId(R.id.item_toggle_icon)).check(matches(isDisplayed()));
+        onView(withId(R.id.item_toggle_name)).check(matches(withText("Toggle: Torch")));
+    }
+
+    public void testBatterySettingAppears() {
+        onView(withId(R.id.searchEditText)).perform(typeText("batter"));
+        onView(withId(R.id.item_setting_icon)).check(matches(isDisplayed()));
+        onView(withId(R.id.item_setting_name)).check(matches(withText("Setting: Battery")));
     }
 }
