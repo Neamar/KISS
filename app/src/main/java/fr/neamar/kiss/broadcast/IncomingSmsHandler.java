5584fc8525d45529cc01b4fc20834824fe12d20d
robberer <robberer@freakmail.de>
2016-03-10 07:49:06 +0100

    remove unused android.util.Log

--- app/src/main/java/fr/neamar/kiss/broadcast/IncomingSmsHandler.java
+++ app/src/main/java/fr/neamar/kiss/broadcast/IncomingSmsHandler.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
-import android.util.Log;
 
 import fr.neamar.kiss.DataHandler;
 import fr.neamar.kiss.KissApplication;
