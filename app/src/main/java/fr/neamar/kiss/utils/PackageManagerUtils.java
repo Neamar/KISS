ccf03aa0784f8220f634e9710c026469ca13d97e
ilmich <ardutu@gmail.com>
2016-05-30 22:28:18 +0200

    Fix startup of phone/sms broadcast receivers

---
+++ app/src/main/java/fr/neamar/kiss/utils/PackageManagerUtils.java
@@ -0,0 +1,24 @@
+package fr.neamar.kiss.utils;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+public class PackageManagerUtils {
+
+    /**
+     * Method to enable/disable a specific component
+     *
+     * @param ctx
+     * @param component
+     * @param enabled
+     */
+    public static void enableComponent(Context ctx, Class component, boolean enabled) {
+        PackageManager pm = ctx.getPackageManager();
+        ComponentName cn =  new ComponentName(ctx, component);
+        pm.setComponentEnabledSetting(cn,
+                enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+}
