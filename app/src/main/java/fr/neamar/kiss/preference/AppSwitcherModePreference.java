c8bcb5a4fcfcae67c2dee0c069f1222740034fc8
computerinfo21 <computerinfo21@users.noreply.github.com>
2016-07-08 10:42:15 -0500

    App Switch Mode preference class

---
+++ app/src/main/java/fr/neamar/kiss/preference/AppSwitcherModePreference.java
@@ -0,0 +1,15 @@
+package fr.neamar.kiss.preference;
+
+import android.content.Context;
+import android.preference.CheckBoxPreference;
+import android.util.AttributeSet;
+
+import fr.neamar.kiss.R;
+
+
+public class AppSwitcherModePreference extends CheckBoxPreference {
+
+    public AppSwitcherModePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
