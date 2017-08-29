46165641c2c3444f0669ecb1a6bb1b33bc04948e
Neamar <neamar@neamar.fr>
2017-08-29 17:13:06 +0200

    Ensure restart when toggling transparency

--- app/src/main/java/fr/neamar/kiss/SettingsActivity.java
+++ app/src/main/java/fr/neamar/kiss/SettingsActivity.java
@@ -30,7 +30,7 @@ public class SettingsActivity extends PreferenceActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener {
 
     // Those settings require the app to restart
-    final static private String requireRestartSettings = "enable-keyboard-workaround force-portrait primary-color";
+    final static private String requireRestartSettings = "enable-keyboard-workaround force-portrait primary-color transparent-search transparent-favorites";
     final static private String requireInstantRestart = "theme notification-bar-color";
 
     private boolean requireFullRestart = false;
