d4c84b6eaaf2e6070559e2af632bae5f2a8cf44a
Neamar <neamar@neamar.fr>
2017-10-27 14:30:41 +0100

    Fix Google Search

--- app/src/main/java/fr/neamar/kiss/result/SearchResult.java
+++ app/src/main/java/fr/neamar/kiss/result/SearchResult.java
@@ -1,12 +1,9 @@
 package fr.neamar.kiss.result;
 
-import android.app.SearchManager;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.net.Uri;
-import android.os.Build;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,6 +13,8 @@ import android.widget.TextView;
 import fr.neamar.kiss.R;
 import fr.neamar.kiss.adapter.RecordAdapter;
 import fr.neamar.kiss.pojo.SearchPojo;
+
+import static fr.neamar.kiss.R.drawable.search;
 
 public class SearchResult extends Result {
     private final SearchPojo searchPojo;
@@ -39,7 +38,7 @@ public class SearchResult extends Result {
         } else {
             String text = context.getString(R.string.ui_item_search);
             appName.setText(enrichText(String.format(text, this.pojo.name, "{" + searchPojo.query + "}"), context));
-            image.setImageResource(R.drawable.search);
+            image.setImageResource(search);
         }
         image.setColorFilter(getThemeFillColor(context), PorterDuff.Mode.SRC_IN);
         return v;
@@ -47,31 +46,10 @@ public class SearchResult extends Result {
 
     @Override
     public void doLaunch(Context context, View v) {
-        boolean exceptionThrown = false;
-        Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
-        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            search.setSourceBounds(v.getClipBounds());
-        }
-        search.putExtra(SearchManager.QUERY, searchPojo.query);
-        if (pojo.name.equals("Google")) {
-            // In the latest Google Now version, ACTION_WEB_SEARCH is broken when used with FLAG_ACTIVITY_NEW_TASK.
-            // Adding FLAG_ACTIVITY_CLEAR_TASK seems to fix the problem.
-            search.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            search.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            try {
-                context.startActivity(search);
-            } catch (ActivityNotFoundException e) {
-                // This exception gets thrown if Google Search has been deactivated:
-                exceptionThrown = true;
-            }
-        }
-        if (exceptionThrown || !pojo.name.equals("Google")) {
-            Uri uri = Uri.parse(searchPojo.url + searchPojo.query);
-            search = new Intent(Intent.ACTION_VIEW, uri);
-            search.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(search);
-        }
+        Uri uri = Uri.parse(searchPojo.url + searchPojo.query);
+        Intent search = new Intent(Intent.ACTION_VIEW, uri);
+        search.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(search);
     }
 
     @Override
