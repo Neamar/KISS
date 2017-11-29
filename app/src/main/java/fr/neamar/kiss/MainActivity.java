640f987c35e290659b52c9f7d071b65ad1a04cb8
TBog <tbog@timmystudios.com>
2017-11-29 10:50:04 +0200

    fix navigation bar overlap
    add better descriptions

--- app/src/main/java/fr/neamar/kiss/MainActivity.java
+++ app/src/main/java/fr/neamar/kiss/MainActivity.java
@@ -1088,8 +1088,7 @@ public class MainActivity extends Activity implements QueryInterface, KeyboardSc
             if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
             {
                 visibility = visibility
-                             //| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                             //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                              | View.SYSTEM_UI_FLAG_FULLSCREEN; // hide status bar
             }
@@ -1099,6 +1098,7 @@ public class MainActivity extends Activity implements QueryInterface, KeyboardSc
             if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
             {
                 visibility = visibility
+                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                              | View.SYSTEM_UI_FLAG_IMMERSIVE
                              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             }
