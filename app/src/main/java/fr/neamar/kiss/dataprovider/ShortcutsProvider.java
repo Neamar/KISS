6cc04024ed995fcc5ade9b4459cd9bba9ffd863c
nmitsou <nikos@neemo.gr>
2018-02-15 02:31:43 +0100

    Adding support for tags on shortcuts.

--- app/src/main/java/fr/neamar/kiss/dataprovider/ShortcutsProvider.java
+++ app/src/main/java/fr/neamar/kiss/dataprovider/ShortcutsProvider.java
@@ -1,5 +1,9 @@
 package fr.neamar.kiss.dataprovider;
 
+import android.util.Pair;
+
+import java.util.List;
+
 import fr.neamar.kiss.loader.LoadShortcutsPojos;
 import fr.neamar.kiss.normalizer.StringNormalizer;
 import fr.neamar.kiss.pojo.Pojo;
@@ -23,10 +27,37 @@ public class ShortcutsProvider extends Provider<ShortcutsPojo> {
 
         for (ShortcutsPojo pojo : pojos) {
             boolean match = fuzzyScore.match(pojo.normalizedName.codePoints, matchInfo);
+            boolean bDisplayNameSet = false;
+            boolean bDisplayTagsSet = false;
             pojo.relevance = matchInfo.score;
 
             if (match) {
-                pojo.setDisplayNameHighlightRegion(matchInfo.getMatchedSequences());
+                List<Pair<Integer, Integer>> positions = matchInfo.getMatchedSequences();
+                try {
+                    pojo.setDisplayNameHighlightRegion(positions);
+                } catch (Exception e) {
+                    pojo.setDisplayNameHighlightRegion(0, pojo.normalizedName.length());
+                }
+                bDisplayNameSet = true;
+            }
+
+            // check relevance for tags
+            if (pojo.normalizedTags != null) {
+                if (fuzzyScore.match(pojo.normalizedTags.codePoints, matchInfo)) {
+                    if (!match || (matchInfo.score > pojo.relevance)) {
+                        match = true;
+                        pojo.relevance = matchInfo.score;
+                        pojo.setTagHighlight(matchInfo.matchedIndices);
+                        bDisplayTagsSet = true;
+                    }
+                }
+            }
+
+            if (match) {
+                if (!bDisplayNameSet)
+                    pojo.displayName = pojo.getName();
+                if (!bDisplayTagsSet)
+                    pojo.displayTags = pojo.getTags();
                 if (!searcher.addResult(pojo))
                     return;
             }
