4d3b823848ca6ffa602b520f2f6c86b795099f19
TBog <tbog@timmystudios.com>
2018-03-06 12:22:38 +0200

    use a custom drawable
    replaced PorterDuffXfermode with a custom drawable

--- app/src/main/java/fr/neamar/kiss/ui/RoundedQuickContactBadge.java
+++ app/src/main/java/fr/neamar/kiss/ui/RoundedQuickContactBadge.java
@@ -2,12 +2,19 @@ package fr.neamar.kiss.ui;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.QuickContactBadge;
 
@@ -19,12 +26,6 @@ import java.lang.reflect.Field;
  * @author kishu27 (http://linkd.in/1laN852)
  */
 public class RoundedQuickContactBadge extends QuickContactBadge {
-
-    /**
-     * This path is used to mask out the outer edges of a circle on this View
-     */
-    private static Bitmap rounder = null;
-    private static Paint xferPaint;
 
     public RoundedQuickContactBadge(Context context) {
         super(context);
@@ -39,6 +40,55 @@ public class RoundedQuickContactBadge extends QuickContactBadge {
     public RoundedQuickContactBadge(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(); //Set our initialization
+    }
+
+    static class RoundedDrawable extends Drawable
+    {
+
+        private final Paint mPaint;
+        private final BitmapShader mBitmapShader;
+        private final RectF mRect;
+
+        RoundedDrawable(Bitmap bitmap) {
+            mBitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+
+            mPaint = new Paint();
+            mPaint.setAntiAlias(true);
+            mPaint.setShader(mBitmapShader);
+            
+            mRect = new RectF(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        }
+
+        @Override
+        protected void onBoundsChange(Rect bounds) {
+            super.onBoundsChange(bounds);
+
+            Matrix m = new Matrix();
+            mBitmapShader.getLocalMatrix(m);
+            m.setScale(bounds.width() / mRect.width(), bounds.height() / mRect.height());
+            mRect.set(bounds);
+            mBitmapShader.setLocalMatrix(m);
+        }
+
+        @Override
+        public void draw(@NonNull Canvas canvas) {
+            canvas.drawRoundRect(mRect, mRect.width() * .5f, mRect.height() * .5f, mPaint);
+        }
+
+        @Override
+        public void setAlpha(int alpha) {
+            mPaint.setAlpha(alpha);
+        }
+
+        @Override
+        public void setColorFilter(@Nullable ColorFilter colorFilter) {
+            mPaint.setColorFilter(colorFilter);
+        }
+
+        @Override
+        public int getOpacity() {
+            return PixelFormat.TRANSLUCENT;
+        }
     }
 
     /**
@@ -61,39 +111,11 @@ public class RoundedQuickContactBadge extends QuickContactBadge {
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
-        if (rounder != null) {
-            if (rounder.getWidth() != w || rounder.getHeight() != h) {
-                rounder.recycle();
-            } else {
-                // we have everything set-up already
-                return;
-            }
+    public void setImageDrawable(@Nullable Drawable drawable) {
+        if ( drawable instanceof BitmapDrawable) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            drawable = new RoundedDrawable(bitmap);
         }
-
-        // We have to make sure our rounded corners have an alpha channel in most cases
-        rounder = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(rounder);
-
-        // We're going to apply this paint eventually using a porter-duff xfer mode.
-        // This will allow us to only overwrite certain pixels. RED is arbitrary. This
-        // could be any color that was fully opaque (alpha = 255)
-        xferPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        xferPaint.setColor(Color.RED);
-
-        // We're just reusing xferPaint to paint a normal looking rounded box
-        canvas.drawRoundRect(new RectF(0, 0, w, h), w, h, xferPaint);
-
-        // Now we apply the 'magic sauce' to the paint
-        xferPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        canvas.drawBitmap(rounder, 0, 0, xferPaint);
+        super.setImageDrawable(drawable);
     }
 }
