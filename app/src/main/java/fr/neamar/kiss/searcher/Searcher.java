413e72992f09af5b476b47247a4d11844bbb298a
Neamar <neamar@neamar.fr>
2018-03-30 00:20:27 +0100

    Removed refresh task

--- app/src/main/java/fr/neamar/kiss/searcher/Searcher.java
+++ app/src/main/java/fr/neamar/kiss/searcher/Searcher.java
@@ -3,15 +3,12 @@ package fr.neamar.kiss.searcher;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.CallSuper;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.concurrent.ExecutorService;
@@ -30,22 +27,14 @@ public abstract class Searcher extends AsyncTask<Void, Result, Void> {
     private static final int DEFAULT_REFRESH_TIMER = 150;
     final WeakReference<MainActivity> activityWeakReference;
     private final PriorityQueue<Pojo> processedPojos;
-    private final RefreshTask refreshTask;
-    // It's better to have a Handler than a Timer. Note that java.util.Timer (and TimerTask) will be deprecated in JDK 9
-    private final Handler handler;
     private long start;
     private final String query;
-    private int refreshCounter;
 
     Searcher(MainActivity activity, String query) {
         super();
         this.query = query;
         this.activityWeakReference = new WeakReference<>(activity);
         this.processedPojos = getPojoProcessor(activity);
-        this.refreshTask = new RefreshTask();
-        this.refreshCounter = 0;
-        // This handler should run on the Ui thread. That's the only thread that can't be blocked.
-        this.handler = new Handler(Looper.getMainLooper());
     }
 
     PriorityQueue<Pojo> getPojoProcessor(Context context) {
@@ -72,21 +61,6 @@ public abstract class Searcher extends AsyncTask<Void, Result, Void> {
         while (this.processedPojos.size() > maxResults)
             this.processedPojos.poll();
 
-        int counter = this.refreshTask.runCounter;
-        // if timer passed, refresh list
-        if (this.refreshCounter < counter) {
-            this.refreshCounter = counter;
-
-            // clone the PriorityQueue so we can extract the pojos in sorted order and still keep them in the original list
-            PriorityQueue<Pojo> queue = new PriorityQueue<>(this.processedPojos);
-            Collection<Result> results = new ArrayList<>(queue.size());
-            while (queue.peek() != null) {
-                results.add(Result.fromPojo(activity, queue.poll()));
-            }
-            // make the adapter update from the main thread
-            publishProgress(results.toArray(new Result[0]));
-        }
-
         return true;
     }
 
@@ -96,7 +70,6 @@ public abstract class Searcher extends AsyncTask<Void, Result, Void> {
         super.onPreExecute();
         start = System.currentTimeMillis();
 
-        this.handler.postDelayed(this.refreshTask, DEFAULT_REFRESH_TIMER);
         displayActivityLoader();
     }
 
@@ -123,8 +96,6 @@ public abstract class Searcher extends AsyncTask<Void, Result, Void> {
 
     @Override
     protected void onPostExecute(Void param) {
-        this.handler.removeCallbacks(this.refreshTask);
-
         MainActivity activity = activityWeakReference.get();
         if (activity == null)
             return;
@@ -157,15 +128,5 @@ public abstract class Searcher extends AsyncTask<Void, Result, Void> {
         void beforeListChange();
 
         void afterListChange();
-    }
-
-    static class RefreshTask implements Runnable {
-        volatile int runCounter = 0;
-
-        @Override
-        @SuppressWarnings("NonAtomicVolatileUpdate")
-        public void run() {
-            runCounter += 1;
-        }
     }
 }
