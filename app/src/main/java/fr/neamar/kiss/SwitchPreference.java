ac02fa71e57209ff883f29bdb6c2dc73b12574d1
Unknown <thetbog@gmail.com>
2018-09-11 16:03:54 +0300

    Force 10 max lines in summary

--- app/src/main/java/fr/neamar/kiss/SwitchPreference.java
+++ app/src/main/java/fr/neamar/kiss/SwitchPreference.java
@@ -2,9 +2,12 @@ package fr.neamar.kiss;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
 
 // https://code.google.com/p/android/issues/detail?id=26194
 // Can be removed once we drop support for KitKat
+// Forced 10 max lines in summary (different Android versions have different values)
 public class SwitchPreference extends android.preference.SwitchPreference {
 
     public SwitchPreference(Context context) {
@@ -17,5 +20,14 @@ public class SwitchPreference extends android.preference.SwitchPreference {
 
     public SwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+
+        View summary = view.findViewById(android.R.id.summary);
+        if (summary instanceof TextView)
+            ((TextView) summary).setMaxLines(10);
     }
 }
