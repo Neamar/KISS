8d7bcdb766bfedeb7f82b5735aa06e1cde93be89
Emmanuel M <emmanuelbendavid@gmail.com>
2018-11-16 23:29:27 -0300

    Remove disk cache

--- app/src/main/java/fr/neamar/kiss/glide/KissGlideModule.java
+++ app/src/main/java/fr/neamar/kiss/glide/KissGlideModule.java
@@ -2,14 +2,16 @@ package fr.neamar.kiss.glide;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.cache.DiskCache;
 import com.bumptech.glide.module.AppGlideModule;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import fr.neamar.kiss.glide.appicon.AppIconModel;
 import fr.neamar.kiss.glide.appicon.AppIconModelLoaderFactory;
 import fr.neamar.kiss.glide.contacts.ContactIconModelLoaderFactory;
@@ -21,5 +23,16 @@ public final class KissGlideModule extends AppGlideModule {
 	public void registerComponents(@NonNull Context context, @NonNull Glide glide, Registry registry) {
 		registry.prepend(AppIconModel.class, Drawable.class, new AppIconModelLoaderFactory(context));
 		registry.prepend(ContactsPojo.class, Drawable.class, new ContactIconModelLoaderFactory(context));
+	}
+
+	@Override
+	public void applyOptions(@NonNull Context context, GlideBuilder builder) {
+		builder.setDiskCache(new DiskCache.Factory() {
+			@Nullable
+			@Override
+			public DiskCache build() {
+				return null;
+			}
+		});
 	}
 }
