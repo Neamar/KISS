1db07c7de3be9299a7021b1fddf4a8deb4e51a94
Emmanuel M <emmanuelbendavid@gmail.com>
2018-11-18 15:55:28 -0300

    Removed redundant BitmapDrawable to Drawable conversion in IconsHandler

--- app/src/main/java/fr/neamar/kiss/IconsHandler.java
+++ app/src/main/java/fr/neamar/kiss/IconsHandler.java
@@ -222,59 +222,7 @@ public class IconsHandler {
             }
         }
 
-        Drawable systemIcon = this.getDefaultAppDrawable(componentName, userHandle);
-        if (systemIcon instanceof BitmapDrawable) {
-            return generateBitmap(systemIcon);
-        }
-        return systemIcon;
-    }
-
-    private Drawable generateBitmap(Drawable defaultBitmap) {
-
-        // if no support images in the icon pack return the bitmap itself
-        if (backImages.size() == 0) {
-            return defaultBitmap;
-        }
-
-        // select a random background image
-        Random r = new Random();
-        int backImageInd = r.nextInt(backImages.size());
-        Bitmap backImage = backImages.get(backImageInd);
-        int w = backImage.getWidth();
-        int h = backImage.getHeight();
-
-        // create a bitmap for the result
-        Bitmap result = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(result);
-
-        // draw the background first
-        canvas.drawBitmap(backImage, 0, 0, null);
-
-        // scale original icon
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(((BitmapDrawable) defaultBitmap).getBitmap(), (int) (w * factor), (int) (h * factor), false);
-
-        if (maskImage != null) {
-            // draw the scaled bitmap with mask
-            Bitmap mutableMask = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            Canvas maskCanvas = new Canvas(mutableMask);
-            maskCanvas.drawBitmap(maskImage, 0, 0, new Paint());
-
-            // paint the bitmap with mask into the result
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
-            canvas.drawBitmap(scaledBitmap, (w - scaledBitmap.getWidth()) / 2, (h - scaledBitmap.getHeight()) / 2, null);
-            canvas.drawBitmap(mutableMask, 0, 0, paint);
-            paint.setXfermode(null);
-        } else { // draw the scaled bitmap without mask
-            canvas.drawBitmap(scaledBitmap, (w - scaledBitmap.getWidth()) / 2, (h - scaledBitmap.getHeight()) / 2, null);
-        }
-
-        // paint the front
-        if (frontImage != null) {
-            canvas.drawBitmap(frontImage, 0, 0, null);
-        }
-
-        return new BitmapDrawable(iconPackres, result);
+        return this.getDefaultAppDrawable(componentName, userHandle);
     }
 
     /**
