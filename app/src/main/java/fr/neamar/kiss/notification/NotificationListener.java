ea72a1fff8bc2f39ee50c0dbfaf468dc4d86b612
Neamar <neamar@neamar.fr>
2019-04-28 16:58:15 +0100

    Synchronise current notifications on start

--- app/src/main/java/fr/neamar/kiss/notification/NotificationListener.java
+++ app/src/main/java/fr/neamar/kiss/notification/NotificationListener.java
@@ -10,7 +10,9 @@ import android.service.notification.StatusBarNotification;
 
 import androidx.annotation.RequiresApi;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
@@ -28,6 +30,36 @@ public class NotificationListener extends NotificationListenerService {
     @Override
     public IBinder onBind(Intent intent) {
         return super.onBind(intent);
+    }
+
+    @Override
+    public void onListenerConnected() {
+        super.onListenerConnected();
+
+        // Build a map of notifications, ordered per package
+        StatusBarNotification[] sbns = getActiveNotifications();
+        Map<String, Set<String>> notificationsByPackage = new HashMap<>();
+        for(StatusBarNotification sbn: sbns) {
+            String packageName = sbn.getPackageName();
+            if(!notificationsByPackage.containsKey(packageName)) {
+                notificationsByPackage.put(packageName, new HashSet<String>());
+            }
+
+            notificationsByPackage.get(packageName).add(Integer.toString(sbn.getId()));
+        }
+
+        // And synchronise this map with our sharedpreferences
+        SharedPreferences.Editor editor = prefs.edit();
+        for(String packageName : prefs.getAll().keySet()) {
+            if(notificationsByPackage.containsKey(packageName)) {
+                editor.putStringSet(packageName, notificationsByPackage.get(packageName));
+            }
+            else {
+                editor.remove(packageName);
+            }
+        }
+
+        editor.apply();
     }
 
     @Override
