184e2f6bc62ba9600fb9ae0c1c2d9da72f90ac0d
Unknown <thetbog@gmail.com>
2020-02-27 14:17:20 +0200

    run clean-up on start-up

--- app/src/main/java/fr/neamar/kiss/forwarder/Widget.java
+++ app/src/main/java/fr/neamar/kiss/forwarder/Widget.java
@@ -125,10 +125,15 @@ class Widget extends Forwarder {
      */
     private void restoreWidget() {
         int currentWidgetId = prefs.getInt(WIDGET_PREF_KEY, -1);
-        if (currentWidgetId == -1) {
-            removeAllWidgets();
-        } else {
+        if(currentWidgetId != -1) {
             addWidgetToLauncher(currentWidgetId);
+        }
+        // kill zombie widgets
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int[] hostWidgetIds = mAppWidgetHost.getAppWidgetIds();
+            for ( int hostWidgetId : hostWidgetIds )
+                if (hostWidgetId != currentWidgetId)
+                    mAppWidgetHost.deleteAppWidgetId(hostWidgetId);
         }
     }
 
@@ -167,13 +172,6 @@ class Widget extends Forwarder {
         while (widgetArea.getChildCount() > 0) {
             AppWidgetHostView widget = (AppWidgetHostView) widgetArea.getChildAt(0);
             removeAppWidget(widget);
-        }
-
-        // kill zombie widgets
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            int[] hostWidgetIds = mAppWidgetHost.getAppWidgetIds();
-            for ( int hostWidgetId : hostWidgetIds )
-                mAppWidgetHost.deleteAppWidgetId(hostWidgetId);
         }
     }
 
