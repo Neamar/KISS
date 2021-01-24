d89ea158d3c3451db638f74e4ff45c4f1f62b736
Neamar <neamar@neamar.fr>
2021-01-24 21:24:35 +0100

    Generate phone screenshots

--- fastlane/generate_graphics.py
+++ fastlane/generate_graphics.py
@@ -10,8 +10,7 @@ import shutil
 LOCALES_PREFIX = './metadata/android/'
 locales = [l.replace(LOCALES_PREFIX, '') for l in sorted(glob.glob(LOCALES_PREFIX + '*'))]
 
-screenshots = ['./graphic_templates/phoneScreenshots/1.svg', './graphic_templates/phoneScreenshots/2.svg', './graphic_templates/phoneScreenshots/3.svg', './graphic_templates/phoneScreenshots/4.svg']
-graphics = ['./graphic_templates/featureGraphic.svg']
+screenshots = sorted(glob.glob('./graphic_templates/phoneScreenshots/*.svg'))
 
 
 def get_last_change(file):
@@ -25,6 +24,33 @@ def get_last_change(file):
     return datetime.datetime.strptime(out.decode('ascii'), r"%Y-%m-%d %H:%M:%S %z")
 
 
+def build_image(template_path, text_path, out_path, placeholder):
+    if not os.path.exists(text_path):
+        return
+
+    text_modified_date = get_last_change(text_path)
+    template_modified_date = get_last_change(template_path)
+    out_modified_date = get_last_change(out_path)
+
+    if text_modified_date <= out_modified_date and template_modified_date <= out_modified_date:
+        return
+
+    print("Generating %s for %s" % (out_path[out_path.rindex('/'):], locale))
+
+    with open(text_path) as f:
+        text = f.readline().strip()
+
+    with open(template_path, 'r') as i:
+        with open('/tmp/out.svg', 'w') as o:
+            for l in i.readlines():
+                l = l.replace(placeholder, text)
+                o.write(l)
+
+    subprocess.check_output(['inkscape', '--export-type=png', '--export-filename=/tmp/out.png', '--export-dpi=96', '/tmp/out.svg'])
+
+    shutil.move("/tmp/out.png", os.getcwd() + '/' + out_path.replace('./', ''))
+
+
 for locale in locales:
     os.makedirs('%s%s/images/phoneScreenshots' % (LOCALES_PREFIX, locale), exist_ok=True)
 
@@ -34,27 +60,13 @@ for locale in locales:
     feature_graphic_template_path = './graphic_templates/featureGraphic.svg'
     feature_graphic_text_path = '%s%s/short_description.txt' % (LOCALES_PREFIX, locale)
     feature_graphic_out_path = '%s%s/images/featureGraphic.png' % (LOCALES_PREFIX, locale)
+    build_image(feature_graphic_template_path, feature_graphic_text_path, feature_graphic_out_path, 't:featureGraphic.subtitle')
 
-    if not os.path.exists(feature_graphic_text_path):
-        continue
-
-    text_modified_date = get_last_change(feature_graphic_text_path)
-    template_modified_date = get_last_change(feature_graphic_template_path)
-    out_modified_date = get_last_change(feature_graphic_out_path)
-
-    if text_modified_date <= out_modified_date and template_modified_date <= out_modified_date:
-        continue
-
-    print("Generating featureGraphic for %s" % locale)
-    with open(feature_graphic_text_path) as f:
-        feature_graphic_text = f.readline().strip()
-
-    with open(feature_graphic_template_path, 'r') as i:
-        with open('/tmp/out.svg', 'w') as o:
-            for l in i.readlines():
-                l = l.replace('t:featureGraphic.subtitle', feature_graphic_text)
-                o.write(l)
-
-    subprocess.check_output(['inkscape', '--export-type=png', '--export-filename=/tmp/out.png', '--export-dpi=96', '/tmp/out.svg'])
-
-    shutil.move("/tmp/out.png", os.getcwd() + '/' + feature_graphic_out_path.replace('./', ''))
+    ######
+    # Screenshots
+    ######
+    for screenshot_template_path in screenshots:
+        screenshot_id = screenshot_template_path[screenshot_template_path.rindex('/')+1:-4]
+        screenshot_text_path = '%s%s/screenshot_%s.txt' % (LOCALES_PREFIX, locale, screenshot_id)
+        screenshot_out_path = '%s%s/images/phoneScreenshots/%s.png' % (LOCALES_PREFIX, locale, screenshot_id)
+        build_image(screenshot_template_path, screenshot_text_path, screenshot_out_path, 't:screenshot.text')
