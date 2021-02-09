056aabd0d46b0a6293eaf1f598490cfc80fbd464
Neamar <neamar@neamar.fr>
2021-02-09 18:46:34 +0100

    Added export option

---
+++ app/src/main/java/fr/neamar/kiss/preference/ExportSettingsPreference.java
@@ -0,0 +1,77 @@
+package fr.neamar.kiss.preference;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import fr.neamar.kiss.R;
+
+import static android.content.Context.CLIPBOARD_SERVICE;
+
+public class ExportSettingsPreference extends DialogPreference {
+
+    public ExportSettingsPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        super.onClick(dialog, which);
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+            // Get default values from XML, to only write changed data
+            SharedPreferences defaultValues = getContext().getSharedPreferences("__default__", Context.MODE_PRIVATE);
+            PreferenceManager.setDefaultValues(getContext(), "__default__", Context.MODE_PRIVATE, R.xml.preferences, true);
+            JSONObject out = new JSONObject();
+            try {
+                for (Map.Entry<String, ?> entry : defaultValues.getAll().entrySet()) {
+                    String key = entry.getKey();
+                    if (entry.getValue() instanceof Boolean) {
+                        boolean currentValue = prefs.getBoolean(key, defaultValues.getBoolean(key, true));
+                        if (currentValue != defaultValues.getBoolean(key, true)) {
+                            out.put(key, currentValue);
+                        }
+                    } else if (entry.getValue() instanceof String) {
+                        String currentValue = prefs.getString(key, defaultValues.getString(key, ""));
+                        if (!currentValue.equals(defaultValues.getString(key, ""))) {
+                            out.put(key, currentValue);
+                        }
+                    } else if (entry.getValue() instanceof Set) {
+                        Set<String> currentValue = prefs.getStringSet(key, new HashSet<String>());
+                        if (!currentValue.equals(defaultValues.getStringSet(key, new HashSet<String>()))) {
+                            out.put(key, new JSONArray(currentValue));
+                        }
+                    } else {
+                        Log.w("Unknown type", entry.getKey() + ":" + entry.getValue());
+                    }
+                }
+
+                ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("kiss", out.toString());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(getContext(), "Settings exported to clipboard", Toast.LENGTH_SHORT).show();
+            } catch(JSONException e) {
+                Toast.makeText(getContext(), "Unable to export preferences", Toast.LENGTH_SHORT).show();
+            }
+            finally {
+                defaultValues.edit().clear().apply();
+            }
+        }
+    }
+}
