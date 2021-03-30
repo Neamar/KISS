611f183a2b88a0922bbcb579dcaee5a804b3833f
Spooner <cmspooner@gmail.com>
2021-03-30 17:26:05 -0400

    Volumes Conversion....but not tested...

--- app/src/main/java/fr/neamar/kiss/dataprovider/simpleprovider/ConversionProvider.java
+++ app/src/main/java/fr/neamar/kiss/dataprovider/simpleprovider/ConversionProvider.java
@@ -16,6 +16,7 @@ import fr.neamar.kiss.searcher.Searcher;
 public class ConversionProvider extends SimpleProvider {
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     final Pattern distConversionRegexp;
+    final Pattern volConversionRegexp;
     private final Pattern numberOnlyRegexp;
 
     public ConversionProvider() {
@@ -23,7 +24,8 @@ public class ConversionProvider extends SimpleProvider {
         //even if the expression is not actually a computable operation.
         String distUnitRegEx = "(miles|mile|mi|feet|foot|ft|inches|inch|in|millimeters|millimeter|mm|kilometers|kilometer|km|meters|meter|m|centimeters|centimeter|cm)";
         distConversionRegexp = Pattern.compile("(^\\-?\\d*.?\\d+)"+distUnitRegEx+"(to)"+distUnitRegEx);
-        String volUnitRegEx = "(gallons|gallon|gal|quart|q)";
+        String volUnitRegEx = "(gallons|gallon|gal|quarts|quart|qrt|qt|pints|pint|pnt|pt|ounces|ounce|oz|liters|liter|l|milliliters|milliliter|ml|cups|cup|cp|tablespoons|tablespoon|tbsp|teaspoones|teaspoon|tsp)";
+        volConversionRegexp = Pattern.compile("(^\\-?\\d*.?\\d+)"+volUnitRegEx+"(to)"+volUnitRegEx);
         numberOnlyRegexp = Pattern.compile("^\\+?[.,()\\d]+$");
     }
 
@@ -32,7 +34,6 @@ public class ConversionProvider extends SimpleProvider {
         Log.v("Spooner", "Conv Query: `" + query);
         String spacelessQuery = query.replaceAll("\\s+", "");
         Matcher m = distConversionRegexp.matcher(spacelessQuery);
-
         if (m.find()) {
             if(numberOnlyRegexp.matcher(spacelessQuery).find()) {
                 return;
@@ -43,7 +44,7 @@ public class ConversionProvider extends SimpleProvider {
             String fromUnit = m.group(2);
             String toUnit = m.group(4);
             BigDecimal toValue;
-            Log.v("Spooner", "m.group() -> " + fromVal + " " + fromUnit + " to " + toUnit);
+            Log.v("Spooner", "Distance -> " + fromVal + " " + fromUnit + " to " + toUnit);
 
             switch (fromUnit){
                 case "miles":
@@ -121,6 +122,132 @@ public class ConversionProvider extends SimpleProvider {
                 case "centimeter":
                 case "cm":
                     toValue = fromValmm.divide(BigDecimal.valueOf(10), 2, RoundingMode.HALF_UP);
+                    break;
+                default:
+                    toValue = BigDecimal.valueOf(0);
+                    break;
+
+            }
+
+            String queryProcessed = query + " = " + toValue;
+            SearchPojo pojo = new SearchPojo("conversion://", queryProcessed, "", SearchPojo.CONVERSION_QUERY);
+
+            pojo.relevance = 19;
+            searcher.addResult(pojo);
+        }
+        m = volConversionRegexp.matcher(spacelessQuery);
+        if (m.find()) {
+            if(numberOnlyRegexp.matcher(spacelessQuery).find()) {
+                return;
+            }
+
+            BigDecimal fromVal = new BigDecimal(Double.parseDouble(Objects.requireNonNull(m.group(1))));
+            BigDecimal fromValml;
+            String fromUnit = m.group(2);
+            String toUnit = m.group(4);
+            BigDecimal toValue;
+            Log.v("Spooner", "Volume -> " + fromVal + " " + fromUnit + " to " + toUnit);
+
+            switch (fromUnit){
+                case "gallons":
+                case "gallon":
+                case "g":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(3785.41));
+                    break;
+                case "quarts":
+                case "quart":
+                case "qrt":
+                case "qt":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(946.353));
+                    break;
+                case "pints":
+                case "pint":
+                case "pnt":
+                case "pt":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(473.176));
+                    break;
+                case "ounces":
+                case "ounce":
+                case "oz":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(29.5735));
+                    break;
+                case "liters":
+                case "liter":
+                case "l":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(1000));
+                    break;
+                case "milliliters":
+                case "milliliter":
+                case "ml":
+                    fromValml = fromVal;
+                    break;
+                case "cups":
+                case "cup":
+                case "cp":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(240));
+                    break;
+                case "tablespoons":
+                case "tablespoon":
+                case "tbsp":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(14.7868));
+                    break;
+                case "teaspoons":
+                case "teaspoon":
+                case "tsp":
+                    fromValml = fromVal.multiply(BigDecimal.valueOf(4.9289317406874));
+                    break;
+                default:
+                    fromValml = BigDecimal.valueOf(0);
+                    break;
+
+            }
+            switch (toUnit){
+                case "gallons":
+                case "gallon":
+                case "g":
+                    toValue = fromValml.divide(BigDecimal.valueOf(3785.41), 2, RoundingMode.HALF_UP);
+                    break;
+                case "quarts":
+                case "quart":
+                case "qrt":
+                case "qt":
+                    toValue = fromValml.divide(BigDecimal.valueOf(946.353), 2, RoundingMode.HALF_UP);
+                    break;
+                case "pints":
+                case "pint":
+                case "pnt":
+                case "pt":
+                    toValue = fromValml.divide(BigDecimal.valueOf(473.176), 2, RoundingMode.HALF_UP);
+                    break;
+                case "ounces":
+                case "ounce":
+                case "oz":
+                    toValue = fromValml.divide(BigDecimal.valueOf(29.5735), 2, RoundingMode.HALF_UP);
+                    break;
+                case "liters":
+                case "liter":
+                case "l":
+                    toValue = fromValml.divide(BigDecimal.valueOf(1000), 2, RoundingMode.HALF_UP);
+                    break;
+                case "milliliters":
+                case "milliliter":
+                case "ml":
+                    toValue = fromVal;
+                    break;
+                case "cups":
+                case "cup":
+                case "cp":
+                    toValue = fromValml.divide(BigDecimal.valueOf(240), 2, RoundingMode.HALF_UP);
+                    break;
+                case "tablespoons":
+                case "tablespoon":
+                case "tbsp":
+                    toValue = fromValml.divide(BigDecimal.valueOf(14.7868), 2, RoundingMode.HALF_UP);
+                    break;
+                case "teaspoons":
+                case "teaspoon":
+                case "tsp":
+                    toValue = fromValml.divide(BigDecimal.valueOf(4.9289317406874), 2, RoundingMode.HALF_UP);
                     break;
                 default:
                     toValue = BigDecimal.valueOf(0);
