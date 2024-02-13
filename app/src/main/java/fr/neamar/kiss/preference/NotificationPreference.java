d941ad0daa894c200e004b8ca0f6bb41bee799b0
marunjar <marunjar@users.noreply.github.com>
2024-02-13 21:42:37 +0100

    fix visibility of notification preferences
    - hide notification preferences based on android version

--- app/src/main/java/fr/neamar/kiss/preference/NotificationPreference.java
+++ app/src/main/java/fr/neamar/kiss/preference/NotificationPreference.java
@@ -1,23 +1,21 @@
 package fr.neamar.kiss.preference;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.preference.DialogPreference;
+import android.provider.Settings;
 import android.util.AttributeSet;
 
 public class NotificationPreference extends DialogPreference {
-    private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
-
     public NotificationPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
-    public void onClick(DialogInterface dialog, int which) {
-        super.onClick(dialog, which);
-        if (which == DialogInterface.BUTTON_POSITIVE) {
-            getContext().startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+    protected void onDialogClosed(boolean positiveResult) {
+        if (positiveResult && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            getContext().startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
         }
     }
 }
