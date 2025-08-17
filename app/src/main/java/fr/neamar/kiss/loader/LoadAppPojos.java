6349c61f0485d905fda5596a4a4c87ee0419ddc7
marunjar <marunjar@users.noreply.github.com>
2025-08-17 20:16:55 +0200

    Revert "simplify exclusion of shown apps of locked profiles"
    This reverts commit 9f10f83538979e368c46cacd81a8faf784678485.

--- app/src/main/java/fr/neamar/kiss/loader/LoadAppPojos.java
+++ app/src/main/java/fr/neamar/kiss/loader/LoadAppPojos.java
@@ -56,8 +56,9 @@ public class LoadAppPojos extends LoadPojos<AppPojo> {
 
             // Handle multi-profile support introduced in Android 5 (#542)
             for (android.os.UserHandle profile : manager.getUserProfiles()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && !manager.isUserUnlocked(profile)) {
-                    continue;
+                LauncherUserInfo info = null;
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                    info = launcherApps.getLauncherUserInfo(profile);
                 }
                 UserHandle user = new UserHandle(manager.getSerialNumberForUser(profile), profile);
                 for (LauncherActivityInfo activityInfo : launcherApps.getActivityList(null, profile)) {
@@ -67,7 +68,18 @@ public class LoadAppPojos extends LoadPojos<AppPojo> {
                     ApplicationInfo appInfo = activityInfo.getApplicationInfo();
                     boolean disabled = PackageManagerUtils.isAppSuspended(appInfo) || isQuietModeEnabled(manager, profile);
                     final AppPojo app = createPojo(user, appInfo.packageName, activityInfo.getName(), activityInfo.getLabel(), disabled, excludedAppList, excludedFromHistoryAppList, excludedShortcutsAppList);
-                    apps.add(app);
+                    if ((android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM)
+                        && (info != null)) {
+                        if (!info.getUserType().equalsIgnoreCase(UserManager.USER_TYPE_PROFILE_PRIVATE)) {
+                            apps.add(app);
+                        } else {
+                            if (!isQuietModeEnabled(manager, profile)) {
+                                apps.add(app);
+                            }
+                        }
+                    } else {
+                        apps.add(app);
+                    }
                 }
             }
         } else {
