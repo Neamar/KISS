b4886ec682da8045aa9e7111c8adc809f37aed39
marunjar <marunjar@users.noreply.github.com>
2025-10-20 21:22:18 +0200

    use fuzzy v2
    - use fuzzy v2 by default
    - add option to change back to v1
    - tweak search: reduce recursion limit

--- app/src/main/java/fr/neamar/kiss/utils/fuzzy/FuzzyFactory.java
+++ app/src/main/java/fr/neamar/kiss/utils/fuzzy/FuzzyFactory.java
@@ -14,10 +14,10 @@ public class FuzzyFactory {
 
     public static FuzzyScore createFuzzyScore(@NonNull Context context, int[] pattern, boolean detailedMatchIndices) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        if (prefs.getBoolean("use-fuzzy-score-v2", false)) {
-            return new FuzzyScoreV2(pattern, detailedMatchIndices);
-        } else {
+        if (prefs.getBoolean("use-fuzzy-score-v1", false)) {
             return new FuzzyScoreV1(pattern, detailedMatchIndices);
+        } else {
+            return new FuzzyScoreV2(pattern, detailedMatchIndices);
         }
     }
 
