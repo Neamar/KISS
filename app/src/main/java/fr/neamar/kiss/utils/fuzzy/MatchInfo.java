be49fcbfec56ba5479d79ad3379c3c9d4c2dc5d7
marunjar <marunjar@users.noreply.github.com>
2025-12-06 20:03:29 +0100

    fix highlight
    - add check for span range
    - ensure that correct positions are highlighted again
      - call `normalized.mapPosition`
      - do *not* use android's phone number normalization

--- app/src/main/java/fr/neamar/kiss/utils/fuzzy/MatchInfo.java
+++ app/src/main/java/fr/neamar/kiss/utils/fuzzy/MatchInfo.java
@@ -1,7 +1,5 @@
 package fr.neamar.kiss.utils.fuzzy;
 
-import android.util.Pair;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,22 +28,7 @@ public class MatchInfo {
         matchedIndices = new ArrayList<>(patternLength);
     }
 
-    public List<Pair<Integer, Integer>> getMatchedSequences() {
-        assert this.matchedIndices != null;
-        // compute pair match indices
-        List<Pair<Integer, Integer>> positions = new ArrayList<>(this.matchedIndices.size());
-        int start = this.matchedIndices.get(0);
-        int end = start + 1;
-        for (int i = 1; i < this.matchedIndices.size(); i += 1) {
-            if (end == this.matchedIndices.get(i)) {
-                end += 1;
-            } else {
-                positions.add(new Pair<>(start, end));
-                start = this.matchedIndices.get(i);
-                end = start + 1;
-            }
-        }
-        positions.add(new Pair<>(start, end));
-        return positions;
+    public List<Integer> getMatchedIndices() {
+        return matchedIndices;
     }
 }
