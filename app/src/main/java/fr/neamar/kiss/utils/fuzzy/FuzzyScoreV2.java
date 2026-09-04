eb0c3e59e12751056111bc35c637ed415bfb9561
marunjar <marunjar@users.noreply.github.com>
2026-09-05 01:15:48 +0200

    improve fuzzy search
    - more bonus for matching first letter

--- app/src/main/java/fr/neamar/kiss/utils/fuzzy/FuzzyScoreV2.java
+++ app/src/main/java/fr/neamar/kiss/utils/fuzzy/FuzzyScoreV2.java
@@ -56,7 +56,7 @@ public class FuzzyScoreV2 implements FuzzyScore {
         adjacency_bonus = 15;
         separator_bonus = 30;
         camel_bonus = 30;
-        first_letter_bonus = 15;
+        first_letter_bonus = 30;
         leading_letter_penalty = -5;
         max_leading_letter_penalty = -30;
         unmatched_letter_penalty = -2;
