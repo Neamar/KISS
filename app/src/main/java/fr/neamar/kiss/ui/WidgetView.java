29ee9d89ac7fe0946bfbe5bb48d14f5da87e4c1c
marunjar <marunjar@users.noreply.github.com>
2026-09-17 20:13:13 +0200

    optimize widgets
    - use separate executor for inflating
    - use setOnLightBackground depending on theme
    - start/stop listening for widget updates according to documentation

--- app/src/main/java/fr/neamar/kiss/ui/WidgetView.java
+++ app/src/main/java/fr/neamar/kiss/ui/WidgetView.java
@@ -1,5 +1,9 @@
 package fr.neamar.kiss.ui;
 
+import static android.content.res.Configuration.UI_MODE_NIGHT_MASK;
+import static android.content.res.Configuration.UI_MODE_NIGHT_NO;
+import static android.content.res.Configuration.UI_MODE_NIGHT_YES;
+
 import android.annotation.SuppressLint;
 import android.appwidget.AppWidgetHostView;
 import android.content.Context;
@@ -10,7 +14,12 @@ import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 
+import androidx.annotation.RequiresApi;
+
 import java.util.Collections;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Source: <a href="https://github.com/willli666/Android-Trebuchet-Launcher-Standalone/blob/master/src/com/cyanogenmod/trebuchet/LauncherAppWidgetHostView.java">LauncherAppWidgetHostView.java</a>
@@ -21,8 +30,30 @@ public class WidgetView extends AppWidgetHostView {
     private float xPos;
     private float yPos;
 
+    private static final int POOL_SIZE = Math.max(Runtime.getRuntime().availableProcessors(), 2);
+    private static final int KEEP_ALIVE = 1;
+
+    /**
+     * An {@link ThreadPoolExecutor} to be used with async task with no limit on the queue size.
+     */
+    public static final ThreadPoolExecutor THREAD_POOL_EXECUTOR;
+
+    static {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(POOL_SIZE, POOL_SIZE, KEEP_ALIVE, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        } else {
+            THREAD_POOL_EXECUTOR = null;
+        }
+    }
+
     public WidgetView(Context context) {
         super(context);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setExecutor(THREAD_POOL_EXECUTOR);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            setOnLightBackground(context.getResources().getConfiguration().uiMode);
+        }
     }
 
     public boolean onInterceptTouchEvent(MotionEvent ev) {
@@ -138,6 +169,16 @@ public class WidgetView extends AppWidgetHostView {
             updateAppWidgetSize(Bundle.EMPTY, Collections.singletonList(new SizeF(widthDips, heightDips)));
         } else {
             updateAppWidgetSize(null, widthDips, heightDips, widthDips, heightDips);
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    public void setOnLightBackground(int uiMode) {
+        switch (uiMode & UI_MODE_NIGHT_MASK) {
+            case UI_MODE_NIGHT_NO:
+                setOnLightBackground(true);
+            case UI_MODE_NIGHT_YES:
+                setOnLightBackground(false);
         }
     }
 }
